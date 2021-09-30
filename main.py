a88ee3f90b7ef8bbc519461f662e9f232c70eee0
devonly <nasawake.am@gmail.com>
2021-10-01 01:16:31 +0900

    - Viteの追加

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ import os
 import sys
 import requests
 import time
+from dotenv import load_dotenv
 
 
 class Salmonia:
     def __init__(self):
         self.iksm_session = os.environ.get("IKSM_SESSION")
         self.api_token = os.environ.get("API_TOKEN")
-        self.last_job_id = int(os.environ.get("LATEST_JOB_NUM"))
+        self.last_job_id = os.environ.get("LATEST_JOB_NUM")
+        self.development = os.environ.get("DEVELOPMENT") == None
 
         # 環境変数がセットされていない
         if self.iksm_session == None:
@@ -23,6 +25,8 @@ class Salmonia:
         if self.last_job_id == None:
             print("NO JOB_ID IS SET")
             sys.exit()
+
+        self.last_job_id = int(self.last_job_id)
 
     def getResultsFromSplatNet2(self):
         for job_num in self.getLatestJobId():
@@ -37,6 +41,10 @@ class Salmonia:
                 print("Not found")
                 continue
             if response.status_code == 200:
+                if self.development:
+                    with open(f"results/{job_num}.json", mode="w") as f:
+                        # JSONを保存
+                        json.dump(response.json(), f)
                 url = "https://salmon-stats-api.yuki.games/api/results"
                 header = {"Content-type": "application/json",
                           "Authorization": "Bearer " + self.api_token}
@@ -56,12 +64,17 @@ class Salmonia:
                     # バイトIDをアップデート
                     os.environ["LATEST_JOB_NUM"] = str(job_num)
                 time.sleep(5)
+            break
 
     def getLatestJobId(self):
         url = "https://app.splatoon2.nintendo.net/api/coop_results"
         response = requests.get(url, cookies=dict(
             iksm_session=self.iksm_session)).json()
         latest_job_id = response["summary"]["card"]["job_num"]
+        if self.development:
+            with open("summary/card.json", mode="w") as f:
+                # JSONを保存
+                json.dump(response["summary"]["card"], f)
         # 新規リザルトがない
         if latest_job_id == self.last_job_id:
             print("No new results")
@@ -71,6 +84,6 @@ class Salmonia:
 
 
 if __name__ == "__main__":
+    load_dotenv()
     salmonia = Salmonia()
-
     salmonia.getResultsFromSplatNet2()
